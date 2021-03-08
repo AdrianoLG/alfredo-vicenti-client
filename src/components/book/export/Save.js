4af49dc4b9d124439938a4c95822a92fe6b90495
AdrianoLG <adri@adri.info>
2021-03-08 21:24:34 +0100

    Add export to Excel

--- src/components/book/export/Save.js
+++ src/components/book/export/Save.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import ReactExport from 'react-export-excel';
+import { connect } from 'react-redux';
 import { Button, Icon } from 'semantic-ui-react';
 
-const SaveBooks = () => (
-  <Button basic className='hov'>
-    <Icon name='download' />
-    Descargar
-  </Button>
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
+
+const SaveBooks = ({ books }) => (
+  <ExcelFile
+    element={
+      <Button basic className='hov'>
+        <Icon name='file excel' />
+        Descargar
+      </Button>
+    }
+  >
+    <ExcelSheet data={books} name='Libros'>
+      <ExcelColumn label='Título' value='title' />
+      <ExcelColumn label='Autor' value='author' />
+      <ExcelColumn label='Categoría' value='category' />
+      <ExcelColumn
+        label='Editorial'
+        value={col => (col.editorial ? col.editorial : '-')}
+      />
+      <ExcelColumn
+        label='¿Prestado?'
+        value={col => (col.lent_to ? col.lent_to : '-')}
+      />
+      <ExcelColumn
+        label='Puntuación'
+        value={col => (col.rating ? col.rating : '-')}
+      />
+    </ExcelSheet>
+  </ExcelFile>
 );
 
-export default SaveBooks;
+const mapStateToProps = state => {
+  return {
+    books: state.books
+  };
+};
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SaveBooks);
